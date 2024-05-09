bec2b8a76ee73b394de22ab9d7fae4049ea598cc
Andrew Hood <ahood@go1.com>
2024-05-09 10:46:09 +0000

    Update CoreServiceProvider.php

--- providers/CoreServiceProvider.php
+++ providers/CoreServiceProvider.php
@@ -164,6 +164,7 @@ class CoreServiceProvider implements ServiceProviderInterface
             if (!isset($c['cacheOptions']['persistent']) || $c['cacheOptions']['persistent'] === true) {
                 $connectionOptions += ['persistent' => 1]; // if we add it as a query parameter, google says this should be 1 instead of true
             }
+            $connectionOptions += ['timeout' => !isset($c['cacheOptions']['timeout']) ? 2 : $c['cacheOptions']['timeout']];
             $connectionOptions += ['read_write_timeout' => !isset($c['cacheOptions']['read_write_timeout']) ? 2 : $c['cacheOptions']['read_write_timeout']];
             $masterDsn .= parse_url($masterDsn, PHP_URL_QUERY) ? '&' : '?';
             $masterDsn .= http_build_query($connectionOptions);
